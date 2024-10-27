6b5d34d494b6116528395be1af30481b3ef45d86
brypro <bperoliva@gmail.com>
2024-10-27 19:09:33 +0000

    Add Producto model to productos/models.py

--- productos/models.py
+++ productos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    marca = models.CharField(max_length=50)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.nombre} ({self.marca})"
