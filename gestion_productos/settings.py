97b0249ae8addc8201c7c0ea39880a898273701c
brypro <bperoliva@google.com>
2024-10-27 21:10:49 +0000

    Refactor database configuration to use PostgreSQL instead of SQLite

--- gestion_productos/settings.py
+++ gestion_productos/settings.py
@@ -75,10 +75,20 @@ WSGI_APPLICATION = "gestion_productos.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Manager',  # Nombre de la base de datos
+        'USER': 'postgres',                  # Usuario de PostgreSQL
+        'PASSWORD': 'postgres',          # Contraseña del usuario
+        'HOST': 'db',                    # Nombre del servicio en docker-compose
+        'PORT': '5432',
     }
 }
 
